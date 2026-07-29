1eecbc0cf46a59ceedc9d5ece406ca16020ebc9c
RafikiDeFaritas <119421045+RafikiDeFaritas@users.noreply.github.com>
2026-07-29 15:39:52 +0100

    feat: ajout config de l'admin

--- space_agency/admin.py
+++ space_agency/admin.py
@@ -1,1 +1,11 @@
 from django.contrib import admin
+from .models import Vaisseau, Mission
+
+admin.site.register(Vaisseau)
+
+@admin.register(Mission)
+class MissionAdmin(admin.ModelAdmin):
+    list_display = ("destination", "date_lancement", "statut", "vaisseau")
+    list_filter = ("statut", "date_lancement")
+    search_fields = ("destination", "vaisseau__nom")
+
